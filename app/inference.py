328be966066da75541b21f7ed4c91b3b163fc257
Neha Patro <neha.patro@gmail.com>
2026-04-23 21:52:43 +0530

    use standard yolov8 model

--- app/inference.py
+++ app/inference.py
@@ -4,7 +4,7 @@ import cv2
 
 class YOLOv12Model:
     def __init__(self, weights_path):
-        self.model = YOLO(weights_path)
+        self.model = YOLO("yolov8n.pt")
 
     def predict(self, img):
         if img is None:
